c73a13c0ee38c42cbe7e2588cd1db34bf32ec340
abhishek <abhigoud2212@gmail.com>
2017-02-18 12:32:46 -0500

    Added DisjointSet.java

---
+++ src/main/java/GraphAlgorithms/DisjointSet.java
@@ -0,0 +1,16 @@
+package GraphAlgorithms;
+
+public class DisjointSet<T> {
+	public void make() {
+
+	}
+
+	public void union() {
+
+	}
+
+	public void find() {
+
+	}
+
+}
--- src/main/java/GraphAlgorithms/Graph.java
+++ src/main/java/GraphAlgorithms/Graph.java
@@ -1,5 +1,28 @@
 package GraphAlgorithms;
 
-public class Graph {
+import java.util.ArrayList;
 
+public class Graph {
+	private int V;
+	private int E;
+	private ArrayList<Integer>[] adj;
+
+	public void Graph(int v) {
+		this.V = v;
+		for (int i = 0; i < v; i++) {
+			adj[i] = new ArrayList<Integer>();
+		}
+	}
+
+	public void numberOfEdges() {
+
+	}
+
+	public boolean validateEdge() {
+		return false;
+	}
+
+	public void addEdge() {
+
+	}
 }
